b98a42708d8fda40526082e726224218fe465abe
Wahdan Hasan <50192463+WahdanHasan@users.noreply.github.com>
2021-03-08 17:11:29 +0400

    backup

--- controllers/my_controller/my_controller.cpp
+++ controllers/my_controller/my_controller.cpp
@@ -7,14 +7,13 @@
 // You may need to add webots include files such as
 // <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
 // and/or to add some other includes
-#include <webots/Robot.hpp>
-#include <webots/distance_sensor.h>
-#include <webots/emitter.h>
-#include <webots/light_sensor.h>
-#include <webots/motor.h>
-#include <webots/position_sensor.h>
-#include <webots/receiver.h>
-#include <webots/robot.h>
+#include <webots/DistanceSensor.hpp>
+#include <webots/Emitter.hpp>
+#include <webots/LightSensor.hpp>
+#include <webots/Motor.hpp>
+#include <webots/PositionSensor.hpp>
+#include <webots/Receiver.hpp>
+#include <webots/Supervisor.hpp>
 #include <iostream>
 
 // All the webots classes are defined in the "webots" namespace
@@ -27,10 +26,20 @@ using namespace webots;
 // a controller program.
 // The arguments of the main function can be specified by the
 // "controllerArgs" field of the Robot node
+
+const int TILES_PER_METER = 4;
+
+#pragma region global variables
+Robot* robot;
+Motor* left_motor;
+Motor* right_motor;
+#pragma endregion
+
+
 int main(int argc, char **argv)
 {
   // create the Robot instance.
-  Robot *robot = new Robot();
+  robot = new Robot();
   // get the time step of the current world.
   int timeStep = (int)robot->getBasicTimeStep();
 
@@ -39,15 +48,16 @@ int main(int argc, char **argv)
   //  Motor *motor = robot->getMotor("motorname");
   //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
   //  ds->enable(timeStep);
+  
+  //WbDeviceTag left_motor = robot->getDeviceTagFromName("left wheel motor");
+  //WbDeviceTag right_motor = robot->getDeviceTagFromName("right wheel motor");
+  left_motor = robot->getMotor("left wheel motor");
+  right_motor = robot->getMotor("right wheel motor");
 
-  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
-  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
-
-
-  wb_motor_set_position(left_motor, INFINITY);
-  wb_motor_set_position(right_motor, INFINITY);
-  wb_motor_set_velocity(left_motor, 0.0);
-  wb_motor_set_velocity(right_motor, 0.0);
+  left_motor->setPosition(INFINITY);
+  right_motor->setPosition(INFINITY);
+  left_motor->setVelocity(0.0);
+  right_motor->setVelocity(0.0);
 
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
@@ -58,14 +68,21 @@ int main(int argc, char **argv)
     //  double val = ds->getValue();
 
     // Process sensor data here.
-      wb_motor_set_velocity(left_motor, 1.0);
-      wb_motor_set_velocity(right_motor, 1.0);
+      left_motor->setVelocity(10.0);
+      right_motor->setVelocity(-10.0);
     // Enter here functions to send actuator commands, like:
     //  motor->setPosition(10.0);
   };
 
   // Enter here exit cleanup code.
 
-  delete robot;
+  CleanUp();
   return 0;
+}
+
+void CleanUp()
+{
+    delete robot;
+    delete left_motor;
+    delete right_motor;
 }
